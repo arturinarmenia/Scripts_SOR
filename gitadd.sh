30b92eb208528be5a0c9c37e97cb4c373d82257c
artur <artur@ubuntu.fpmislata.local>
2013-12-04 00:37:54 +0100

    add artur.md

--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 git add $1
 git commit -a -m "add $1"
-git push -u origin master
+git push -u $origen master
+if [ "$2" == "github" ]; then
+	origen=origin
+else
+	if [ "$2" == "bitbucket" ]; then
+		origen=originbit
+	else
+		if [ "$2" == "" ]; then
+			origen=origin
+		else
+			echo "Parametros incorrectos"
+			return 0
+		fi
+	fi
+fi
