30130f0667dc0105e76199fe0627d9ce57b97b96
Artur <artur@Ubuntu.fpmislata.local>
2014-01-10 16:29:44 +0100

    Script de añadir usuario

--- addusuario.sh
+++ addusuario.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
-if [ "$1" == "" ]; then
-echo "escribe un usuario"
+usuario=$1
+if [ "$usuario" == "" ]; then
+    echo "escribe un usuario"
 else
-useradd $1
-mkdir /home/$1
-chown $1:$1 /home/$1
-passwd $1
+    echo "Añadiendo el Usuario $usuario"
+    useradd $usuario
+    echo "Creando Directorio personal en /home/$usuario"
+    mkdir /home/$usuario
+    chown $usuario:$usuario /home/$usuario
+    echo "Copiando ficheros de /etc/skel"
+    cp /etc/skel /home/$usuario
+    echo "Escriba la contraseña del Usuario $usuario"
+    passwd $usuario
+    echo "Esciba la configuracion de $usuario"
+    echo -n "Nombre Completo "
+    read
+    echo -n "Ciudad "
+    read
+    echo -n "Telegono del Trabajo "
+    read
+    echo -n "Telefono de Casa "
+    read
+    echo -n "otra infomacion "
+    read
+    echo -n "¿Es correcta esta informacion? [S/n] "
+    read respuesta
+    if [ "$respuesta" == "S" ]; then
+        : 
+    else
+        if [ "$respuesta"  == "n" ]; then
+            echo "Cancelando cracion de Usuario"
+            userdel $usuario
+            rm -R -f /home/$usuario
+        else
+            echo "Respuesta erronea se tomara como afirmativa"
+        fi
+    fi
 fi
