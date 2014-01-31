51ab47d0e1ba30c748703309e51db082983dfeeb
Artur <artur@Ubuntu.fpmislata.local>
2014-01-31 15:29:12 +0100

    add quantum.sh

--- quantum.sh
+++ quantum.sh
@@ -1,6 +1,6 @@
 #bin/bash!
 pid=$1
-nice1=$(ps -eo pid,nice | grep "^ *$pid" | cut -c6-10)
+nice1=$(ps -eo pid,nice | grep "^ *$pid " | cut -c6-10)
 if [ $nice1 -ge "0" ]; then
   q=$[(20-$nice1)*5]
 else
