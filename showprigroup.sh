c2d84e606f94f81d2033124fddece29b1899b579
Artur <artur@Ubuntu.fpmislata.local>
2014-01-31 16:19:00 +0100

    add showprigroup.sh

--- showprigroup.sh
+++ showprigroup.sh
@@ -1,7 +1,7 @@
 #bin/bash!
 usuario=$1
 grupo=$(cat /etc/group | grep -P "^$usuario:" | cut -d ":" -f4 )
-if [ "$usuario" == "" ]; then
+if [ "$grupo" == "" ]; then
  echo "Escriba un usuario correcto"
 else
  echo "$grupo"
